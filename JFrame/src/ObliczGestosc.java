7546594f3d3ab6880b17730987de0a4cf506a31e
Meczka <patryk.stachowski@op.pl>
2017-01-06 12:51:48 +0100

    add

--- JFrame/src/ObliczGestosc.java
+++ JFrame/src/ObliczGestosc.java
@@ -11,11 +11,7 @@ import javax.swing.JTextArea;
 
 
 public class ObliczGestosc extends JDialog implements ActionListener{
-<<<<<<< HEAD
 	private JLabel masaL,objetoscL,wynikL,outputL;
-=======
-	private JLabel masaL,objetoscL,wynikL,koniecL;
->>>>>>> branch 'master' of https://github.com/Meczka/PraceDomowe.git
 	private JComboBox masaC,objetoscC,wynikC;
 	private JTextArea masaT,objetoscT;
 	private JButton jb;
@@ -34,11 +30,6 @@ public class ObliczGestosc extends JDialog implements ActionListener{
 		objetoscC = new JComboBox();
 		objetoscT = new JTextArea();
 		objetoscL =  new JLabel("Objetoœæ:");
-		
-		koniecL = new JLabel("test");
-		
-		koniecL.setBounds(200,100,70,20);
-		koniecL.setText("test");
 		
 		wynikC = new JComboBox();
 		
@@ -82,11 +73,7 @@ public class ObliczGestosc extends JDialog implements ActionListener{
 		add(wynikC);
 		add(jb);
 		add(wynikL);
-<<<<<<< HEAD
 		add(outputL);
-=======
-		add(koniecL);
->>>>>>> branch 'master' of https://github.com/Meczka/PraceDomowe.git
 		jb.addActionListener(this);
 
 		
